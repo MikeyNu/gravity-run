e0dbc6e0973149c56355227c43c23f85e991d2ff
Michael Ndhlovu <mikeynuking@gmail.com>
2026-08-04 09:01:00 +0200

    Version simulation fairness contract

--- packages/game-config/src/version.ts
+++ packages/game-config/src/version.ts
@@ -1,3 +1,3 @@
-export const simulationVersion = 'gravity-run-sim/0.2.0';
-export const configurationVersion = 'gravity-run-config/0.2.0';
-export const configurationHash = 'gr-v0.2-course-score-physics';
+export const simulationVersion = 'gravity-run-sim/0.4.0';
+export const configurationVersion = 'gravity-run-config/0.4.0';
+export const configurationHash = 'gr-v0.4-latch-coyote-scrape-energy';
